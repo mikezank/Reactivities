c82125e0feeed77b2d75500d2cc750a74288a2ad
mikezank <mike.zanchettin@gmail.com>
2025-02-17 06:01:44 -0600

    Add better UI styling

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -3,13 +3,13 @@ import App from "../layout/App";
 import { HomePage } from "../../features/home/HomePage";
 import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
 import { ActivityForm } from "../../features/activities/form/ActivityForm";
-import { ActivityDetail } from "../../features/activities/details/ActivityDetail";
+import { ActivityDetailPage } from "../../features/activities/details/ActivityDetailPage";
 
 export const router = createBrowserRouter([
     { path: '/', element: <App />, children: [
         { path: '', element: <HomePage /> },
         { path: 'activities', element: <ActivityDashboard /> },
-        { path: 'activities/:id', element: <ActivityDetail /> },
+        { path: 'activities/:id', element: <ActivityDetailPage /> },
         { path: 'createActivity', element: <ActivityForm key='create' /> },
         { path: 'manage/:id', element: <ActivityForm /> },
     ]}
